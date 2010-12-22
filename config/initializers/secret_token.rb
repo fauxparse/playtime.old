4f87add3704571574ca2c93d364b57d93df63b92
Matt Powell <fauxparse@gmail.com>
2010-12-22 15:25:56 +1300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Playtime::Application.config.secret_token = 'e580e96380dbaa33a17a29bd8dc726dc1087e7a86e5cf983cbbd77fd606d9307c0b1bbf9df774fa9382d5723f23a2258bba0495a37072bee406661c374059224'
