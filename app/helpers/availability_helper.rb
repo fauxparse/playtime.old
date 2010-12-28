a6255d251db7a8f26787786e80a01dad7bce3b6a
Matt Powell <fauxparse@gmail.com>
2010-12-29 09:45:37 +1300

    style existing interface for iPhone

--- app/helpers/availability_helper.rb
+++ app/helpers/availability_helper.rb
@@ -1,6 +1,6 @@
 module AvailabilityHelper
   def weekend(dates)
-    "#{dates.first.strftime(dates.first.month == dates.last.month ? "%e" : "%e %B")} &amp; #{dates.last.strftime("%e %B, %Y")}".html_safe
+    "#{dates.first.strftime(dates.first.month == dates.last.month ? "%e" : "%e %b")} &amp; #{dates.last.strftime("%e %b")}".html_safe
   end
   
   def availability_for(show, jester)
@@ -11,7 +11,7 @@ module AvailabilityHelper
     end
     name = "shows[#{show.date.to_s(:db)}][availability][]"
     id = "shows_#{show.date.to_s(:db)}_availability_#{jester.id}"
-    title = show.date.strftime("%A")
+    title = show.date.strftime("%a")
     (check_box_tag(name, jester.id, available, :title => title, :id => id) + label_tag(id, title)).html_safe
   end
 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,18 @@
 var toolTips = new Tips('[title]', {
   offset: { x:-8, y:16 }
 });
+
+if (window.orientation) {
+  var setOrientation = function() {
+    var orient = Math.abs(window.orientation) === 90 ? 'landscape' : 'portrait';
+    var cl = document.body.className;  
+    cl = cl.replace(/portrait|landscape/, orient);  
+    document.body.className = cl;  
+  }
+  window.addEventListener('load', setOrientation, false);  
+  window.addEventListener('orientationchange', setOrientation, false);
+}
+
+window.addEventListener('load', function(){
+  setTimeout(scrollTo, 0, 0, 1);
+}, false);
