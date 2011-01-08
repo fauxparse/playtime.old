b8a63cefffc08b86d943f6602bbcecec91477df5
Matt Powell <fauxparse@gmail.com>
2011-01-09 12:46:11 +1300

    next/previous show links

--- app/helpers/shows_helper.rb
+++ app/helpers/shows_helper.rb
@@ -1,2 +1,14 @@
+# encoding: UTF-8
+
 module ShowsHelper
+  def previous_show_link(show)
+    show = Show.before(show.date).order(:date).last
+    content_tag :li, link_to("« #{show.date.to_s(:long)}", show_path(*show.params)) if show.present?
+  end
+
+  def next_show_link(show)
+    show = Show.after(show.date + 1).order(:date).first
+    content_tag :li, link_to("#{show.date.to_s(:long)} »", show_path(*show.params)) if show.present?
+  end
+
 end
