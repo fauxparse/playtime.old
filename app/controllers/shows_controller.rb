678bc7b68621c804b04a2fa0054d8084170406c2
Matt Powell <fauxparse@gmail.com>
2011-01-31 20:11:33 +1300

    date bug

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -45,7 +45,7 @@ protected
         if params[:day]
           Date.civil params[:year].to_i, params[:month].to_i, params[:day.to_i]
         else
-          Date.civil params[:year].to_i, params[:month.to_i], 1]
+          Date.civil params[:year].to_i, params[:month].to_i, 1
         end
       else
         Date.civil params[:year].to_i, 1, 1
